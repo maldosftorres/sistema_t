647e5b50fe79b41f14d39b0545846ded6719d681
Fernando <maldosftorres@gmail.com>
2024-03-11 18:02:28 -0300

    se agrega boton registro en login

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('auth.login');
 });
+
+
+Route::get('/w', function () {
+    return view('welcome');
+});
+
 
 
 
